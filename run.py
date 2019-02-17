9ea918423e3249be381313d9d492d2b102b1f1e9
xiangyin55 <37323325+xiangyin55@users.noreply.github.com>
2019-02-17 23:34:07 +0800

    Update run.py

--- run.py
+++ run.py
@@ -28,24 +28,7 @@ from flask import request
 from flask import url_for
 import random,json
 
-
-
 app = Flask(__name__)
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
 
 def save_ips():
 
