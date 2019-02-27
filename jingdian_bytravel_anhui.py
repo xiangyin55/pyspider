5b6f1906aa22bef8aca842b4ace1173227ff1a88
xiangyin55 <37323325+xiangyin55@users.noreply.github.com>
2019-02-28 03:31:30 +0800

    Create jingdian_bytravel_anhui.py

---
+++ jingdian_bytravel_anhui.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Created on 2019-02-19 22:26:17
+# Project:  jingdian_bytravel_anhui_v1
+
+from pyspider.libs.base_handler import *
+import json,re,random
+
+def get_proxy():
+    data = open('/var/mee99_ips.txt', 'r').read().split('\n')
+    return random.choice(data)
+
+class Handler(BaseHandler):
+    crawl_config = {
+        'process_time_limit' : 60
+    }
+
+    @every(minutes=24 * 60)
+    def on_start(self):
+        #self.crawl('http://wap.bytravel.cn/', callback=self.index_page)
+        self.crawl('http://wap.bytravel.cn/view/index120_list.html', callback=self.list_page)
+        
+    @config(age=10 * 24 * 60 * 60)
+    def index_page(self, response):
+        for each in response.doc('li > a').items():
+            open ('/var/bytravel_list_anhui.txt','a').write(each.attr.href+"\n")
+            if re.match("http://wap.bytravel.cn/view/\w+", each.attr.href, re.U):
+                self.crawl(each.attr.href.replace(".html","_list.html"), callback=self.list_page)
+               
+
+    @config(priority=2)
+    def list_page(self, response):
+        for each in response.doc("nav a").items():
+            if u'下一页' in each.text():
+                next = each.attr.href
+                print (next)
+                self.crawl(next, callback=self.list_page)
+        for each in response.doc('#titlename a').items():
+            self.crawl(each.attr.href, callback=self.detail_page)
+            
+        
+    
+    @config(priority=2)
+    def detail_page(self, response):
+
+        area_s = response.doc("#mainbao a").text()
+        area_full = area_s.replace(" ",".").replace(u"首页",u"中国").replace(u"旅游",u"") if area_s else ""
+        return {
+            "url": response.url,
+            "name": response.doc('h1').text(),
+            "summary":  response.doc('article').text()[0:response.doc('article').text().find(u"下一景区：")],
+            "area_full" :  area_full,
+            "area" : area_full.split(".")[-1] if area_full else "",
+            "image": response.doc('article > div > a > img').attr.src,
+            "qualification":  response.doc('.f14b').text()    
+        }
