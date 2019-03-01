70e32d68c519dc188cc93d4232f670d6145a48bc
xiangyin55 <37323325+xiangyin55@users.noreply.github.com>
2019-03-01 20:31:57 +0800

    Update jingdian_mfw_anhui.py

--- jingdian_mfw_anhui.py
+++ jingdian_mfw_anhui.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-# Created on 2019-02-22 16:30:14
-# Project: jingdian_mfw_anhui_v3
+# Created on 2019-03-01 09:27:32
+# Project: mfw_hefei_v2
+
+
 
 from pyspider.libs.base_handler import *
 import json
@@ -26,25 +28,28 @@ def random_proxy():
 def gen_headers(url):
     return { 
         "Referer" : url,
-        'User-Agent': random_agent(),
+        "User-Agent": random_agent(),
     }
 
 class Handler(BaseHandler):
     crawl_config = {
-        'process_time_limit' : 60
+        'process_time_limit' : 60,
+        'itag' : "1.0.10" ,
     }
 
 
     @every(minutes=24 * 60)
     def on_start(self):
+        poi = "10793"
         
-        jdata = '{"iMddid":"12719","iPage":"1","iTagId":"0","sAct":"KMdd_StructWebAjax|GetPoisByTag"}';
+        jdata = '{"iMddid":"'+poi+'","iPage":"1","iTagId":"0","sAct":"KMdd_StructWebAjax|GetPoisByTag"}';
         param = json.loads( jm( jdata ) )
         url = "https://www.mafengwo.cn/ajax/router.php"
         self.crawl(url,params=param,callback=self.index_page)
 
     @config(age=10 * 24 * 60 * 60)
     def index_page(self, response):
+   
 
         list_doc = pq(response.json["data"]["list"])  #print (list_doc.html())
         page_doc = pq(response.json["data"]["page"])  #print (page_doc.html())
@@ -54,13 +59,13 @@ class Handler(BaseHandler):
 
             self.crawl("https://www.mafengwo.cn"+d.attr.href, callback=self.detail_page_poi,proxy=random_proxy())
 
-
-        ipage = page_doc(".pg-next").attr("data-page")
-        jdata = '{"iMddid":"12719","iPage":"'+ipage+'","iTagId":"0","sAct":"KMdd_StructWebAjax|GetPoisByTag"}';
-        param = json.loads( jm( jdata ) )
-        url = "https://www.mafengwo.cn/ajax/router.php"
-        self.crawl(url,params=param,callback=self.index_page)
-#        self.crawl(url,params=param,callback=self.index_page,proxy=random_proxy())
+        if  page_doc(".pg-next"):
+            ipage = page_doc(".pg-next").attr("data-page")
+            jdata = '{"iMddid":"10793","iPage":"'+ipage+'","iTagId":"0","sAct":"KMdd_StructWebAjax|GetPoisByTag"}';
+            param = json.loads( jm( jdata ) )
+            url = "https://www.mafengwo.cn/ajax/router.php"
+            self.crawl(url,params=param,callback=self.index_page)
+    #        self.crawl(url,params=param,callback=self.index_page,proxy=random_proxy())
 
     @config(priority=2)
     def detail_page_poi(self, response):
@@ -78,7 +83,7 @@ class Handler(BaseHandler):
         self.crawl(poiCommentListApi, callback=self.detail_page_Comment,headers=gen_headers(response.url))
         
         poiSubPoiApi = 'http://pagelet.mafengwo.cn/poi/pagelet/poiSubPoiApi?params={"poi_id":"'+poi+'","type":"3"}'
-        self.crawl(poiSubPoiApi, callback=self.index_page_subpoi,headers=gen_headers(response.url),save={'pid':poi})
+        self.crawl(poiSubPoiApi, callback=self.index_page_subpoi,headers=gen_headers(response.url),save={'parent':poi})
         
         if not response.doc(".mod-detail").html() is None :
             open("/var/mfwdata/{}_detail.txt".format(poi),'w').write(response.doc(".mod-detail").html().encode('utf8'))
@@ -86,6 +91,7 @@ class Handler(BaseHandler):
 
         return {
             "poi": int(poi),
+
             "url": response.url,
             "area" : area[0].replace(u'景点','') if area else "",
             "name" : response.doc("h1").text(),
@@ -125,22 +131,22 @@ class Handler(BaseHandler):
     @config(age=10 * 24 * 60 * 60)
     def index_page_subpoi(self, response):
         print (response.json)
-        pid = response.save['pid']
+        parent = response.save['parent']
 
         result = response.json["data"]
         if result["html"] =="" :  return
         list_doc = pq(result["html"])
         for d in list_doc('a[href^="/poi"]').items():   
-            self.crawl("https://www.mafengwo.cn"+d.attr.href, callback=self.detail_page_subpoi,save={'go':d.find('em').text(),'pid':pid})
+            self.crawl("https://www.mafengwo.cn"+d.attr.href, callback=self.detail_page_subpoi,save={'go':d.find('em').text(),'parent':parent},proxy=random_proxy())
 #            self.crawl("https://www.mafengwo.cn"+d.attr.href, callback=self.detail_page_subpoi,save={'go':d.find('em').text(),'pid':pid},proxy=random_proxy())
 
         page_json = result['controller_data']
         if page_json['hasMore'] :
             page = page_json['curPage'] + 1
             poi = page_json['poi_id']
-            poiSubPoiApi = 'http://pagelet.mafengwo.cn/poi/pagelet/poiSubPoiApi?params={"poi_id":"'+str(poi)+'","type":"3"},"page":'+str(page)+'}'
+            poiSubPoiApi = 'http://pagelet.mafengwo.cn/poi/pagelet/poiSubPoiApi?params={"poi_id":"'+str(poi)+'","type":3,"page":'+str(page)+'}'
             refer = "https://www.mafengwo.cn/poi/{}.html".format(str(poi))
-            self.crawl(poiSubPoiApi, callback=self.index_page_subpoi,headers=gen_headers(refer),save={'pid':poi})
+            self.crawl(poiSubPoiApi, callback=self.index_page_subpoi,headers=gen_headers(refer),save={'parent':poi})
 
             
             
@@ -163,7 +169,7 @@ class Handler(BaseHandler):
 
             "poi": int(poi),
             "go" : int(response.save["go"]),
-            "p_poi": response.save["pid"],
+            "parent": int(response.save["parent"]),
             "url": response.url,
             "area" : response.doc(".cur").text(),
             "image": "" if  ( img is None) else img.split('?')[0] ,
@@ -180,13 +186,7 @@ class Handler(BaseHandler):
             return
         result["update"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         client = pymongo.MongoClient('127.0.0.1')
-        db = client["scenics_anhui"]
-        poi = db["mafengwo"]
-        if poi.find_one({'poi': result.get('poi')}):
-            poi.update({'poi': result.get('poi')}, {'$set': result})
-        else:
-            poi.insert_one(result)
-            
+        db = client["scenics_hefei"]
+        poi = db["mfw"]
+        poi.update({'poi': result.get('poi')}, {'$set': result},True,False)
         
-            
-            
